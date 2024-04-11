e0e8a801d8a8396944f0d8a3b85da93d956d4620
goldipl <mgodlewski92@gmail.com>
2024-04-11 10:42:10 +0200

    1st commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  // Increase counter
+  const increase = () => {
+    console.log("dupa");
+    setCounter((count) => count + 1);
+  };
+
+  // Decrease counter
+  const decrease = () => {
+    setCounter((prevCount) => Math.max(prevCount - 1, 0));
+  };
+
+  // Reset counter
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <>
+      <div className="select-none text-6xl font-semibold flex place-items-center my-6">
+        {counter}
+      </div>
+      <div className="pointer-events-auto text-2xl font-semibold flex place-items-center mt-8">
+        <button
+          className="flex select-none pointer-events-auto mx-3 pointer rounded-lg border border-transparent px-5 py-4 bg-gray-100 border-gray-300 hover:border-neutral-400  hover:bg-neutral-500/10"
+          onClick={increase}
+        >
+          +
+        </button>
+        <button
+          className="flex select-none pointer-events-auto mx-3 pointer rounded-lg border border-transparent px-5 py-4 bg-gray-100 border-gray-300 hover:border-neutral-400  hover:bg-neutral-500/10"
+          onClick={decrease}
+        >
+          -
+        </button>
+        <button
+          className="flex select-none pointer-events-auto mx-3 pointer rounded-lg border border-transparent px-5 py-4 bg-gray-100 border-gray-300 hover:border-neutral-400  hover:bg-neutral-500/10"
+          onClick={reset}
+        >
+          Reset
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
